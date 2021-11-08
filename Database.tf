3c1ff6e2b3e316ed84f4ef86c48179cb27fcfc49
omarnoubair <36126062+omarnoubair@users.noreply.github.com>
2021-11-08 14:07:51 +0100

    Update Database.tf

--- Database.tf
+++ Database.tf
@@ -9,7 +9,7 @@
 
 # Create Database Subnet Group
 # terraform aws db subnet group
-resource "aws_db_subnet_group" "database-subnet-group" {
+/*resource "aws_db_subnet_group" "database-subnet-group" {
   name         = "database subnets"
   subnet_ids   = [aws_subnet.private-subnet-3.id, aws_subnet.private-subnet-2.id,aws_subnet.private-subnet-1.id]
   description  = "Subnets for Database Instance"
@@ -59,4 +59,4 @@ resource "aws_db_instance" "database-instance-dev" {
   db_subnet_group_name      = aws_db_subnet_group.database-subnet-group.name
   multi_az                  = "${var.multi-az-deployment}"
   vpc_security_group_ids    = [aws_security_group.sg_oregon.id]
-}
+}*/
