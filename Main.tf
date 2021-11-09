7ad7fb435d4e69be2716e58e6d5170fac25ecd66
omarnoubair <36126062+omarnoubair@users.noreply.github.com>
2021-11-09 10:31:30 +0100

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 # definition provider
 provider "aws" {
-  access_key = var.aws_access_key
+ /* access_key = var.aws_access_key
   secret_key = var.aws_secret_key
-  region = "${var.vpc-region}"
+  region = "${var.vpc-region}"*/
 }
